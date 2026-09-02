88dd4b28a4be2a53f1bf1b76281afdd6350c8a20
koalakids-app <cp1.koalakids@outlook.fr>
2026-09-02 17:29:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker — Stocks Pédagogiques
 // Stratégie ultra-simple : cache minimal, réseau prioritaire
-const CACHE_NAME = 'stocks-v5';
+const CACHE_NAME = 'stocks-v6';
 
 self.addEventListener('install', event => {
   event.waitUntil(
